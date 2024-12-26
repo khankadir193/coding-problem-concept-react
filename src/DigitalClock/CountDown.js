ab0f8b67498e1070ef9cadba98ae920bb1c4ace4
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-12-26 22:13:45 +0530

    countDown

--- src/DigitalClock/CountDown.js
+++ src/DigitalClock/CountDown.js
@@ -1,47 +1,63 @@
 import React, { useEffect, useState } from 'react'
 
 const CountDown = () => {
-    const [days,setDays] = useState('');
-    const [seconds,setSeconds] = useState('');
-    const [minutes,setMinutes] = useState('');
-    const [hours,setHours] = useState('');
+    // const [days,setDays] = useState('');
+    const [seconds, setSeconds] = useState('00');
+    const [minutes, setMinutes] = useState('00');
+    const [hours, setHours] = useState('00');
 
     const btnStyle = {
-        width:'20%',
-        background:'black',
-        color:'white',
-        outline:'none',
-        height:'5vh',
-        borderRadius:'3vw',
+        width: '20%',
+        background: 'black',
+        color: 'white',
+        outline: 'none',
+        height: '5vh',
+        borderRadius: '3vw',
         // padding:'1vw'
-        fontSize:'1vw',
-        fontStyle:'italic',
-        position:'absolute',
-        zIndex:'1000'
+        fontSize: '1vw',
+        fontStyle: 'italic',
+        position: 'absolute',
+        zIndex: '1000'
     }
 
-    useEffect(()=>{
-        let countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
-        const now = new Date().getTime();
-        const distance = countDownDate - now;
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    useEffect(() => {
 
-        setDays(days);
-        setSeconds(seconds);
-        setMinutes(minutes);
-        setHours(hours);
-    },[]);
+        console.log('hi.....');
+
+        const interval = setInterval(() => {
+            let hour = hours;
+            let min = minutes;
+            let sec = `0${Number(seconds)}` + 1;
+
+            sec = sec < 10 ? `0${sec}` : `${sec}`;
+            if(sec === 60){
+                sec = '00';
+                min = Number(minutes) + 1 
+                min = min < 10 ? `0${min}` : min;
+                if(min === 60){
+                    min = '00';
+                    hour = Number(hours) + 1;
+                    hour = hour < 10 ? `0${hour}` : hour;
+                }
+            }
+            setSeconds(sec);
+            setMinutes(min);
+            setHours(hour);
+        },10);
+
+
+        return ()=>{
+            clearInterval(interval);
+        }
+    }, [hours,minutes,seconds]);
 
     const handleTimer = (event) => {
-        console.log('eventValue...',event);
+        console.log('eventValue...', event);
     };
 
     return (
         <div>
-           <h2>{`${days}:${hours}:${minutes}:${seconds}`}</h2>
+            <h2>{`${hours}:${minutes}:${seconds}`}</h2>
             <button style={btnStyle} onClick={handleTimer}>TimerStart</button>
         </div>
     )
